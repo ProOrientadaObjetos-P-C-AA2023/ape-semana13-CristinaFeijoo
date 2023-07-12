18cc5d2319333d64e32ad33952a8c0d3ef7228ce
CristinaFeijoo <115591738+CristinaFeijoo@users.noreply.github.com>
2023-07-12 01:39:36 -0500

    TALLER13

--- problema01/src/paquete005/Principal.java
+++ problema01/src/paquete005/Principal.java
@@ -4,7 +4,12 @@
  */
 package paquete005;
 
+import paquete001.Persona;
+import paquete002.Ciudad;
 import paquete003.BilleteraPagos;
+import paquete004.*;
+
+import java.util.ArrayList;
 
 /**
  *
@@ -12,7 +17,22 @@ import paquete003.BilleteraPagos;
  */
 public class Principal {
     public static void main(String[] args) {
-        BilleteraPagos pago = new BilleteraPagos();
-        System.out.println(pago);
+        BilleteraPagos pagos=new BilleteraPagos();
+        pagos.persona=new Persona("Nowo","Oreo",34,"oncecuatro",new Ciudad("Loja"));
+        pagos.mes="Oct";
+        ArrayList<PagoAguaPotable> aguaCasa =new ArrayList<>();
+        ArrayList<PagoAguaPotable> aguaComercio =new ArrayList<>();
+        ArrayList<PagoLuzElectrica> luzCasa=new ArrayList<>();
+        ArrayList<PagoLuzElectrica> luzComercio=new ArrayList<>();
+        ArrayList<PagoPredial> propiedadCasa1=new ArrayList<>();
+        ArrayList<PagoPredial> propiedadCasa2=new ArrayList<>();
+        ArrayList<PagoTelefonoConvencional> casa=new ArrayList<>();
+        ArrayList<PagoTelefonoConvencional> finca=new ArrayList<>();
+
+        GastosTotales gastosPagos= new GastosTotales(aguaCasa,aguaComercio,luzCasa,luzComercio,propiedadCasa1,propiedadCasa2,casa,finca);
+
     }
+
+
+
 }

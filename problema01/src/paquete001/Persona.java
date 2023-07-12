2e7217b58f87abb9486b3fd57042dbd687716143
CristinaFeijoo <115591738+CristinaFeijoo@users.noreply.github.com>
2023-07-12 01:37:56 -0500

    TALLER13

--- problema01/src/paquete001/Persona.java
+++ problema01/src/paquete001/Persona.java
@@ -11,10 +11,71 @@ import paquete002.Ciudad;
  * @author reroes
  */
 public class Persona {
-    String nombre;
-    String apellido;
-    int edad;
-    String cedula;
-    public Ciudad ciudad;
-    
+    private String nombre;
+    private String apellido;
+    private int edad;
+    private String cedula;
+    private Ciudad ciudad;
+
+    public Persona(String nombre, String apellido, int edad, String cedula, Ciudad ciudad) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.cedula = cedula;
+        this.ciudad = ciudad;
+    }
+
+    public Persona() {
+
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public Ciudad getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(Ciudad ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona  " +
+                "nombre  " + nombre + '\'' +
+                ", apellido " + apellido + '\'' +
+                ", edad " + edad +
+                ", cedula '" + cedula + '\'' +
+                ", ciudad " + ciudad ;
+    }
 }
